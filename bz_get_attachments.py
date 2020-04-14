8a20c6d1b4d6d0df51e33ea4309f3bc113916a60
Mark Michelson <mark.michelson@gmail.com>
2020-04-14 13:21:24 -0400

    Add bz_get_attachments.py
    Script to retrieve attachments from an issue at bugzilla.redhat.com

---
+++ bz_get_attachments.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+# Retrieve attachments from a Bugzilla issue.
+# Usage:
+#   bz_get_attachments.py <bug_number>
+#
+# This script will prompt the user for login
+# credentials for redhat.bugzilla.com, and then will
+# retrieve the attachments for <bug_number>.
+#
+# Attachments are saved to $HOME/bz_issues/<bug_number>/
+# If the directory does not exist, it will be created.
+
+import bugzilla
+import os
+import sys
+from pathlib import Path
+
+URL = "bugzilla.redhat.com"
+bzapi = bugzilla.Bugzilla(URL)
+
+if len(sys.argv) < 2:
+    print("Please include a BZ bug number")
+    sys.exit(1)
+
+bugno = sys.argv[1]
+
+if not bzapi.logged_in:
+    bzapi.interactive_login()
+
+print(f"Retrieving bug {bugno}")
+try:
+    bug = bzapi.getbug(bugno)
+except Exception:
+    print(f"Failed to retrieve bug {bugno}. Exiting.")
+    sys.exit(1)
+
+path = Path(Path.home(), Path('bugzilla'), Path(bugno))
+
+print(f"Creating directory {path} if it does not already exist")
+os.makedirs(path, exist_ok=True)
+
+print(f"Retrieving attachments")
+atts = bug.get_attachments()
+for att in atts:
+    data = att['data']
+    if hasattr(data, "data"):
+        content = data.data
+    else:
+        import base64
+        content = base64.decode(data)
+
+    with open(f'{os.fspath(path)}/{att["file_name"]}', 'wb') as fi:
+        print(f'Writing {fi.name}')
+        fi.write(content)
