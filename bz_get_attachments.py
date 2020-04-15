9f6ccf9b96cfca7188d2ba692f52c558107bce2a
Mark Michelson <mmichels@redhat.com>
2020-04-15 16:09:23 -0400

    Get rid of XMLRPC version of bz_get_attachments
    This removes the original bz_get_attachments.py and renames
    bz_get_attachements_rest.py to bz_get_attachments.py.
    
    The documentation at the top of the file has also been updated to
    reflect the change from an interactive login to the use of API key.

--- bz_get_attachments.py
+++ bz_get_attachments.py
@@ -4,20 +4,34 @@
 # Usage:
 #   bz_get_attachments.py <bug_number>
 #
-# This script will prompt the user for login
-# credentials for redhat.bugzilla.com, and then will
-# retrieve the attachments for <bug_number>.
+# This script retrieves the attachments for <bug_number>.
 #
 # Attachments are saved to $HOME/bugzilla/<bug_number>/
 # If the directory does not exist, it will be created.
+#
+# This script uses a bugzilla API key for authentication. The
+# API key should be stored in a config file like so:
+#
+# [bugzilla.redhat.com]
+# api_key = <key>
+#
+# You can generate an API key at
+# https://bugzilla.redhat.com/userprefs.cgi?tab=apikey
+#
+# The script searches the following files for configuration
+#   ~/.config/bugzillarc
+#   ~/.bugzillarc
+#   $PWD/.bugzillarc
+#
+# The files are searched in that order.
 
-import bugzilla
-import os
+
+import requests
 import sys
+import os
 from pathlib import Path
-
-URL = "bugzilla.redhat.com"
-bzapi = bugzilla.Bugzilla(URL)
+import base64
+import configparser
 
 if len(sys.argv) < 2:
     print("Please include a BZ bug number")
@@ -25,14 +39,31 @@ if len(sys.argv) < 2:
 
 bugno = sys.argv[1]
 
-if not bzapi.logged_in:
-    bzapi.interactive_login()
+config = configparser.ConfigParser()
+paths = [
+    Path(Path.home(), '.config', 'bugzillarc'),
+    Path(Path.home(), '.bugzillarc'),
+    Path('.bugzillarc'),
+]
 
-print(f"Retrieving bug {bugno}")
+config.read(paths)
+domain = 'bugzilla.redhat.com'
+
 try:
-    bug = bzapi.getbug(bugno)
-except Exception:
-    print(f"Failed to retrieve bug {bugno}. Exiting.")
+    api_key = config[domain]['api_key']
+except KeyError:
+    print("No configured api_key found")
+    sys.exit(1)
+
+s = requests.Session()
+s.headers.update({'api_key': api_key})
+
+url = f'https://{domain}'
+
+r = s.get(f'{url}/rest/bug/{bugno}/attachment')
+
+if r.status_code != 200:
+    print("Failed to retrieve attachments")
     sys.exit(1)
 
 path = Path(Path.home(), Path('bugzilla'), Path(bugno))
@@ -40,17 +71,8 @@ path = Path(Path.home(), Path('bugzilla'), Path(bugno))
 print(f"Creating directory {path} if it does not already exist")
 os.makedirs(path, exist_ok=True)
 
-print("Retrieving attachments")
-atts = bug.get_attachments()
-for att in atts:
-    data = att['data']
-    if hasattr(data, "data"):
-        content = data.data
-    else:
-        import base64
-        content = base64.b64decode(data)
-
+for att in r.json()['bugs'][bugno]:
     write_path = Path(path, Path(att['file_name']))
     with open(write_path, 'wb') as fi:
         print(f'Writing {fi.name}')
-        fi.write(content)
+        fi.write(base64.b64decode(att['data']))
