f42c907a74f42d72b58f099389fe9046e08bd036
Mark Michelson <mmichels@redhat.com>
2020-06-22 15:46:43 -0400

    Add bz_search_bugs.py
    This is a command line utility that allows for searching for bugs. The
    command line tool currently only offers a small subset of search
    options, because they're the only fields I've cared to search for.
    However, the actual utility function included here allows for searching
    based on any parameters.

---
+++ bz_search_bugs.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+# Search for BZ issues and return their bug numbers.
+# Usage:
+#   bz_search_bugs.py --summary="keywords"
+#
+# This script retrieves the attachments for <bug_number>.
+#
+# Attachments are saved to $HOME/bugzilla/<bug_number>/
+# If the directory does not exist, it will be created.
+#
+# This script uses a bugzilla API key for authentication. The
+# API key should be stored in a config file like so:
+#
+# [bugzilla.redhat.com]
+# api_key = <key>
+#
+# You can generate an API key at
+# https://bugzilla.redhat.com/userprefs.cgi?tab=apikey
+#
+# The script searches the following files for configuration
+#   ~/.config/bugzillarc
+#   ~/.bugzillarc
+#   $PWD/.bugzillarc
+#
+# The files are searched in that order.
+
+from argparse import ArgumentParser
+from utilities import open_session, REST_URL
+
+
+def search_bz(session, params):
+    r = session.get(f'{REST_URL}/bug', params=params)
+    r.raise_for_status()
+    return r.json()['bugs']
+
+
+if __name__ == '__main__':
+    search_fields = [
+        "summary",
+        "status",
+        "product",
+        "component",
+    ]
+    parser = ArgumentParser(description="Search for bugs")
+    for field in search_fields:
+        parser.add_argument(f'--{field}',
+                            help=f'Bugzilla {field} field')
+
+    args = parser.parse_args()
+    params = {}
+    for field in search_fields:
+        try:
+            setattr(args, field, getattr(args, field).split(','))
+            params[field] = getattr(args, field)
+        except AttributeError:
+            # Command line didn't set one of the fields. Not a problem.
+            pass
+
+    session = open_session()
+    bugs = search_bz(session, params)
+    for bug in bugs:
+        print(bug['id'])
